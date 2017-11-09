afeed0a62a0c16e30929292b0c31eeba4c0b56b3
septimiu <septimiu.mitu@gmail.com>
2017-11-09 23:29:51 +0200

    fixed master py

--- master.py
+++ master.py
@@ -13,7 +13,7 @@ dbname='/var/www/templog.db'
 # Get the last action
 def get_last_action():
     connection=sqlite3.connect(dbname)
-    cursor1=conn.cursor()
+    cursor1=connection.cursor()
     cursor1.execute("SELECT action FROM actions order by timestamp desc limit 1"):
     result=cursor1.fetchone()
     connection.close()
