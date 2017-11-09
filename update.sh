2a3fc15c3e624c71bbcaf07766ca2b11af354c62
septimiu <septimiu.mitu@gmail.com>
2017-11-09 21:53:27 +0200

    testing execution

--- update.sh
+++ update.sh
@@ -2,3 +2,4 @@
 echo updating the control-the-boiler based on Github
 git -C /home/pi/control-the-boiler/ pull
 python /home/pi/control-the-boiler/monitor.py
+python /home/pi/control-the-boiler/controlrelay.py
